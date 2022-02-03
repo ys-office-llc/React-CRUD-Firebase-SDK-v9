0586d0d2558d3b65a1fa191089822c3060370c33
YS office LLC <yusuke.sato@ys-office.llc>
2022-02-03 19:04:23 +0900

    ユーザ一覧の取得

---
+++ firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID 
+}
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
+export const storage = getStorage();
+
+export default app;
